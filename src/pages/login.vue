75328fe19f4a7785ca8519de8c9db109d3312f18
huangchen <1440557925@qq.com>
2017-08-09 21:42:12 +0800

    init project

---
+++ src/pages/login.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <div>
+            用户名：<input type="text"/>
+            密码：<input type="password"/>
+            <input type="button" value="登录"/>
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
