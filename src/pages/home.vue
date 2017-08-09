fe78e31e2d17cde9ff0856541f17cce952c96a14
huangchen <1440557925@qq.com>
2017-08-09 23:40:44 +0800

    完成首页

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,9 +1,180 @@
 <template>
-    <div>
-        home
+    <!-- 布局的盒子-->
+    <div class="aal-layout">
+        <!--头部banner-->
+        <div class="aal-banner">
+            <img src="../assets/images/bg2-small.png">
+        </div>
+        <!--导航模块-->
+        <div class="aal-main clearfix">
+            <div class="logo clearfix">
+                <img src="../assets/images/logo.jpg" alt="">
+            </div>
+            <ul class="clearfix">
+                <li><a href="#">我的订单</a></li>
+                <li><a href="#">上新商品</a><i>|</i></li>
+                <li><a href="#">全部商品</a><i>|</i></li>
+            </ul>
+            <div class="aal-search">
+                <form>
+                    <span class="icon-search"></span>
+                    <input type="search" placeholder="搜索商品"/>
+                </form>
+            </div>
+        </div>
+        <!--产品展示模块-->
+        <div class="aal-product">
+            <ul>
+                <li><a href="#"><img src="../assets/images/bg1.jpg" alt=""/></a></li>
+                <li><a href="#"><img src="../assets/images/bg2.jpg" alt=""/></a></li>
+                <li><a href="#"><img src="../assets/images/bg3.jpg" alt=""/></a></li>
+                <li><a href="#"><img src="../assets/images/bg3.jpg" alt=""/></a></li>
+                <li><a href="#"><img src="../assets/images/bg3.jpg" alt=""/></a></li>
+            </ul>
+            <ol>
+                <li class="current"></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ol>
+        </div>
+        <!--明星产品-->
+        <div class="aal-star">
+            <ul>
+                <li>
+                    <a href="#">
+                        <img src="../assets/images/sk2.jpg" alt=""/>
+                        <p>SK-ll</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src="../assets/images/xsl.jpg" alt=""/>
+                        <p>希思黎</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src="../assets/images/HR.jpg" alt=""/>
+                        <p>HR</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src="../assets/images/esld.jpg" alt=""/>
+                        <p>雅诗兰黛</p>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <!--产品列表-->
+        <div class="aal-list clearfix">
+            <ul>
+                <li>
+                    <a href="#">
+                        <p>美容仪器</p>
+                        <img src="../assets/images/ln-big.jpg" alt="">
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <p>阿玛尼</p>
+                        <img src="../assets/images/amn-big.jpg" alt="">
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <p>海蓝之谜</p>
+                        <img src="../assets/images/hlzm-big.jpg" alt="">
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <p>SK-II</p>
+                        <img src="../assets/images/sk-big.jpg" alt="">
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <p>希思黎</p>
+                        <img src="../assets/images/xsl-big.jpg" alt="">
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <p>赫连娜</p>
+                        <img src="../assets/images/HR1.jpg" alt="">
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <p>雅诗兰黛</p>
+                        <img src="../assets/images/es1.jpg" alt="">
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <p>肌肤之钥</p>
+                        <img src="../assets/images/cpb.jpg" alt="">
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <p>黛珂</p>
+                        <img src="../assets/images/dk.jpg" alt="">
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <p>茵芙纱</p>
+                        <img src="../assets/images/ipsa.jpg" alt="">
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <!--底部导航-->
+        <div class="aal-footnav ">
+            <ul class="clearfix">
+                <li>
+                    <a href="#"><img src="../assets/images/home.png" alt=""></a>
+                </li>
+                <li>
+                    <a href="#">全部商品</a>
+                </li>
+                <li>
+                    <a href="#">购物车</a>
+                </li>
+                <li>
+                    <a href="#">我的订单</a>
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 
 <script>
+    // ajax 使用官方推荐的 axios
+    import axios from 'axios'
 
+    export default {
+        data () {
+            return{
+                listArr: [],
+            }
+        },
+        created () {
+            this.loadList();
+        },
+        methods: {
+            loadList: function() {
+                console.log("初始化加载数据开始...");
+            }
+        }
+    }
 </script>
+
+<style>
+    @import '/src/assets/css/base.css';
+    @import '/src/assets/css/index.css';
+</style>
--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,13 +1,96 @@
 <template>
-    <div>
-        <div>
-            用户名：<input type="text"/>
-            密码：<input type="password"/>
-            <input type="button" value="登录"/>
+    <div class="login-layout">
+        <!-- 登陆的头部-->
+        <div class="login-header">
+            <h3>邮箱登陆</h3>
+        </div>
+        <!--登陆主体-->
+        <div class="login-main">
+            <div class="login-main-top">
+                <h2>凹凹啦</h2>
+                <label><input type="text" placeholder="凹凹啦邮箱账号"></label><br>
+                <label> <input type="text" placeholder="密码"></label><br>
+                <label><input type="button" value="登陆" class="login-btn"></label><br>
+            </div>
+            <div class="login-main-footer">
+                <a href="#" class="login-reg">去注册</a>
+                <a href="#" class="login-pwd">忘记密码</a>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
 
+    // ajax 使用官方推荐的 axios
+    import axios from 'axios'
+
+    export default {
+        data () {
+            return{
+                listArr: [],
+            }
+        },
+        created () {
+            this.loadList();
+        },
+        methods: {
+            loadList: function() {
+                console.log("初始化加载数据开始...");
+            }
+        }
+    }
 </script>
+
+<style>
+    @import '/src/assets/css/base.css';
+
+    .login-layout {
+        width: 100%;
+        max-width: 640px;
+        min-width: 320px;
+        margin: 0 auto;
+        background-color: #fff;
+
+    }
+    .login-layout .login-header{
+        max-width: 640px;
+        min-width: 320px;
+        width: 100%;
+        height: 60px;
+        background-color: rgba(201,21,35,0);
+        margin: 0 auto;
+        /*padding: 5px 50px 0 150px;*/
+        position: relative;
+        border-bottom: 1px solid #eeeeee;
+    }
+    .login-layout .login-header h3{
+        width: 100%;
+        height: 100%;
+        padding-top: 15px;
+        text-align: center;
+
+    }
+    .login-main{
+        position: relative;
+        padding: 10px 50px 10px 50px;
+    }
+    .login-main .login-main-top h2{
+        text-align: center;
+        color: #d8505c;
+        margin: 20px 0;
+    }
+    .login-main .login-main-top input{
+        width: 100%;
+        height: 30px;
+        border: 1px solid #cccccc;
+        padding-left: 5px;
+        margin-bottom: 10px;
+    }
+    .login-main-top .login-btn{
+        margin-bottom: 20px;
+    }
+    .login-main-footer .login-pwd{
+        float: right;
+    }
+</style>
