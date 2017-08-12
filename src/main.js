19e62b250db5db84864a762e9d2547f1f30d7668
huangchen <1440557925@qq.com>
2017-08-12 19:39:13 +0800

    添加axios

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
+import axios from "axios";
 import App from './App.vue'
 import router from "./router"
 import MintUI from 'mint-ui'
 import 'mint-ui/lib/style.css'
 
-Vue.use(MintUI)
+Vue.use(MintUI);
+
+axios.defaults.baseURL = 'http://127.0.0.1:8090'; // 配置请求根路径
 
 new Vue({
     router: router,
--- src/pages/allgoods.vue
+++ src/pages/allgoods.vue
@@ -2,79 +2,19 @@
     <div class="allgoods">
         <h2>全部商品</h2>
         <div class="aal-goods">
-            <div class="goods-item">
+            <div class="goods-item" v-for="good in goods">
                 <a href="#">
                     <img src="../assets/images/esld.jpg" alt=""  class="logo-img">
                 </a>
                 <div class="iteminfo">
-                    <p>雅诗兰黛</p>
+                    <p>{{good.name}}</p>
                     <p class="new">
                         <b>￥</b>
-                        <em>268.00</em>
+                        <em>{{good.currentPrice}}</em>
                     </p>
                     <p class="old">
                         <i>￥</i>
-                        <del>1268.00</del>
-                    </p>
-                    <div class="shopping-cart clearfix " >
-                        <a href="#"><img src="../assets/images/shoppingcar.png" alt=""></a>
-                    </div>
-                </div>
-
-            </div>
-            <div class="goods-item">
-                <a href="#">
-                    <img src="../assets/images/esld.jpg" alt=""  class="logo-img">
-                </a>
-                <div class="iteminfo">
-                    <p>雅诗兰黛</p>
-                    <p class="new">
-                        <b>￥</b>
-                        <em>268.00</em>
-                    </p>
-                    <p class="old">
-                        <i>￥</i>
-                        <del>1268.00</del>
-                    </p>
-                    <div class="shopping-cart clearfix " >
-                        <a href="#"><img src="../assets/images/shoppingcar.png" alt=""></a>
-                    </div>
-                </div>
-
-            </div>
-            <div class="goods-item">
-                <a href="#">
-                    <img src="../assets/images/esld.jpg" alt=""  class="logo-img">
-                </a>
-                <div class="iteminfo">
-                    <p>雅诗兰黛</p>
-                    <p class="new">
-                        <b>￥</b>
-                        <em>268.00</em>
-                    </p>
-                    <p class="old">
-                        <i>￥</i>
-                        <del>1268.00</del>
-                    </p>
-                    <div class="shopping-cart clearfix " >
-                        <a href="#"><img src="../assets/images/shoppingcar.png" alt=""></a>
-                    </div>
-                </div>
-
-            </div>
-            <div class="goods-item">
-                <a href="#">
-                    <img src="../assets/images/esld.jpg" alt=""  class="logo-img">
-                </a>
-                <div class="iteminfo">
-                    <p>雅诗兰黛</p>
-                    <p class="new">
-                        <b>￥</b>
-                        <em>268.00</em>
-                    </p>
-                    <p class="old">
-                        <i>￥</i>
-                        <del>1268.00</del>
+                        <del>{{good.originalPrice}}</del>
                     </p>
                     <div class="shopping-cart clearfix " >
                         <a href="#"><img src="../assets/images/shoppingcar.png" alt=""></a>
@@ -90,19 +30,28 @@
 </template>
 
 <script>
+    import axios from 'axios'
     import Navbar from '../components/navbar.vue'
 
     export default {
         data () {
             return{
-
+                goods: []
             }
         },
         created () {
-
+            this.getGoods()
         },
         methods: {
-
+            getGoods: function () {
+                let that = this;
+                let url = '/api/v1/goods';
+                axios.get(url)
+                    .then(function (response) {
+                        console.log(response.data);
+                        that.goods = response.data;
+                    });
+            }
         },
         components :{
             Navbar
